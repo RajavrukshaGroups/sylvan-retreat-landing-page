bbee12be1a6ff359069552ff94318bbdb1470b8d
anupsr <sranup007@gmail.com>
2026-08-13 17:20:58 +0530

    new chnages

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 
-//const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+// const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 const API_BASE_URL ='https://rrplserver.rajavrukshagroup.in'
 /**
  * Submits enquiry data to the backend endpoint.
